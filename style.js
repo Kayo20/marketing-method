56c5126f2378515200bf177eea1bf40381ceb618
Kayo20 <108233417+Kayo20@users.noreply.github.com>
2022-07-02 15:57:58 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,6 @@
+const togglebutton = document.getElementsByClassName("toggle-button"){0}
+const nav ul = document. getElementsByClassName("nav ul"){0}
+
+togglebutton.addEventlistener("click", ()=>{
+    nav ul.classList.toggle("active")
+})
